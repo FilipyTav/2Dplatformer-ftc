7292b1cbd740da8da06a9ad9d184a7951ccf94b1
Filipy <filipytav17@gmail.com>
2025-09-13 23:48:44 -0300

    Player agora tem um pouco mais de tempo para pular fora de uma plataforma

--- code/scripts/player.gd
+++ code/scripts/player.gd
@@ -6,28 +6,59 @@ extends CharacterBody2D
 @export var jump_speed = -300
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var coyote_timer = $CoyoteTimer
+
+var coyote_frames: int = 6  # How many in-air frames to allow jumping
+var coyote: bool = false  # Track whether it's coyote time or not
+var last_floor: bool = false  # Last frame's on-floor state
+var jumping: bool = false
 
 # To sync with rigid nodes
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta: float) -> void:
-	### Movement
+func _ready() -> void:
+	coyote_timer.wait_time = coyote_frames / 60.0
 
+func _physics_process(delta: float) -> void:
 	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y += jump_speed
+	get_input(delta)
+
+	move_and_slide()
+
+	if (is_on_floor() and jumping):
+		jumping = false
+
+	if !is_on_floor() and last_floor and !jumping:
+		coyote = true
+		coyote_timer.start()
+
+	last_floor = is_on_floor()
+
+
+func _on_coyote_timer_timeout() -> void:
+	coyote = false
+
+func get_input(delta: float) -> void:
+	### Movements
 
 	# Get input direction - -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
 
-	if (direction != 0):
+	if (direction):
 		velocity.x = lerp(velocity.x, direction * speed, acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, friction)
+
+	# Handle jump
+	if (jumping):
+		animated_sprite.play("jump")
+
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote):
+		jumping = true
+		velocity.y += jump_speed
 
 	### Visuals
 
@@ -39,11 +70,7 @@ func _physics_process(delta: float) -> void:
 
 	# Play animation
 	if (is_on_floor()):
-		if (direction == 0):
+		if (!direction):
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
-
-	move_and_slide()
