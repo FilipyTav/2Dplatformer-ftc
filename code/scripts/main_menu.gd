8c6a326517c048c2ad0b9a0aaa07bd60b91b8df2
Filipy <filipytav17@gmail.com>
2025-09-19 10:56:28 -0300

    Menu de opções adicionado

--- code/scripts/main_menu.gd
+++ code/scripts/main_menu.gd
@@ -2,10 +2,15 @@ extends Control
 
 @onready var main_btns: VBoxContainer = $MainBtns
 @onready var options: Panel = $Options
+@onready var bg: Panel = $Bg
+
 
 func _ready() -> void:
 	main_btns.visible = true
 	options.visible = false
+	bg.visible = true
+	$Bg/Cock.visible = true
+	
 
 func _on_start_btn_pressed() -> void:
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
@@ -13,6 +18,7 @@ func _on_start_btn_pressed() -> void:
 func _on_options_btn_pressed() -> void:
 	main_btns.visible = false
 	options.visible = true
+	$Bg/Cock.visible = false
 
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
