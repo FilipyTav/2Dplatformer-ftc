305a397e09432c07d06b2d53c79af426b4044031
Filipy <filipytav17@gmail.com>
2025-09-19 22:23:57 -0300

    Imagem da corrente adicionada

--- code/scripts/game_manager.gd
+++ code/scripts/game_manager.gd
@@ -7,3 +7,5 @@ var score: int = 0
 func add_point() -> void:
 	score += 1
 	score_label.text = "Poins: " + str(score)
+
+# TODO: make the options menu its own scene, and finish the pause functionality
