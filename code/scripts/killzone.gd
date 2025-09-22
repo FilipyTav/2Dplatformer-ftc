fe1a06af8168cb00cc7d130bed971c32eeb7cd15
Filipy <filipytav17@gmail.com>
2025-09-22 20:19:04 -0300

    Barra de vida agora funcional

--- code/scripts/killzone.gd
+++ code/scripts/killzone.gd
@@ -1,12 +1,42 @@
 extends Area2D
 
-@onready var timer = $Timer
+@onready var timer: Timer = $Timer
+@onready var dmg_cd: Timer = $DmgCooldown
+
+# Enemy stats
+@export var damage: int = 1
+@export var attack_cooldown: float = 1.0  # Time between each attack
+
+signal dealt_damage(value)
+
+func _ready() -> void:
+	dmg_cd.wait_time = attack_cooldown
 
 func _on_body_entered(body:Node2D) -> void:
-	print('Death')
-	Engine.time_scale = .5
-	timer.start()
+	# Make sure the body is the player (check its type)
+	print(body)
+	if body.is_in_group("player"):  # Make sure player is in the "player" group
+		if !dmg_cd.is_stopped():
+			print("Runnin")
+			return
+
+		deal_damage(body)
+
+# Function to deal damage
+func deal_damage(body: Node) -> void:
+	print("Take dmg")
+	# You can connect this to a signal or directly affect the player's health
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+
+	# Emit a signal that damage has been dealt (if needed elsewhere)
+	emit_signal("dealt_damage", damage)
+	dmg_cd.start()
+
 
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1
 	get_tree().reload_current_scene()
+
+func _on_dmg_cooldown_timeout() -> void:
+	pass
