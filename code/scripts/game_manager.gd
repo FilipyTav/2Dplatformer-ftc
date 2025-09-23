ac0a0cb5a658cd2ec48fed9a250265d3a42f22fe
Filipy <filipytav17@gmail.com>
2025-09-23 10:47:54 -0300

    Adicionada funcionalidade de pause

--- code/scripts/game_manager.gd
+++ code/scripts/game_manager.gd
@@ -1,11 +1,24 @@
 extends Node
 
 @onready var score_label: Label = $"../Labels/ScoreLabel"
+@onready var options: Panel = $"../UI/Options"
 
 var score: int = 0
+
+func _ready() -> void:
+	options.hide()
+	var quit_btn = options.get_node("BackBtn")
+	quit_btn.text = "Quit"
+	quit_btn.connect("pressed", self._on_quit_btn_pressed)
 
 func add_point() -> void:
 	score += 1
 	score_label.text = "Poins: " + str(score)
 
-# TODO: make the options menu its own scene, and finish the pause functionality
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		options.hide() if options.visible else options.show()
+		get_tree().paused = !get_tree().paused
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit()
