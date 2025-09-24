c0970fead6531ca15af056f01d2deebd23060f89
Filipy <filipytav17@gmail.com>
2025-09-23 21:21:33 -0300

    Menu de opções melhorado

--- code/scripts/game_manager.gd
+++ code/scripts/game_manager.gd
@@ -8,9 +8,9 @@ var score: int = 0
 
 func _ready() -> void:
 	options.hide()
-	var quit_btn = options.get_node("BackBtn")
-	quit_btn.text = "Quit"
-	quit_btn.connect("pressed", self._on_quit_btn_pressed)
+	var menu_btn = options.get_node("BackBtn")
+	menu_btn.text = "Menu"
+	menu_btn.connect("pressed", self._on_menu_btn_pressed)
 	options_animplay = options.get_node("AnimationPlayer")
 
 func add_point() -> void:
@@ -21,8 +21,9 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		options.hide() if options.visible else options.show()
 		options_animplay.play("blur") if options.visible else options_animplay.play_backwards("blur")
-		
+
 		get_tree().paused = !get_tree().paused
 
-func _on_quit_btn_pressed() -> void:
-	get_tree().quit()
+func _on_menu_btn_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
